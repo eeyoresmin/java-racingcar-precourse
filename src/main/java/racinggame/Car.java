c003d3eb57deedefd943cf961f6b0f68ac9f95ed
민형 정 <kingofhadess@naver.com>
2021-10-12 12:37:59 +0900

    feat Car 객체 이동 및 상태 표시 추가

--- src/main/java/racinggame/Car.java
+++ src/main/java/racinggame/Car.java
@@ -1,6 +1,12 @@
 package racinggame;
 
+import nextstep.utils.Randoms;
+
 public class Car {
+	private static final int START_NUM = 0;
+	private static final int END_NUM = 9;
+	private static final int BASIS_NUM = 4;
+	private static final String DISPLAY_FORMAT = "%s : %s";
 
 	private CarName carName;
 	private CarPosition carPosition;
@@ -19,6 +25,13 @@ public class Car {
 	}
 
 	public void move() {
+		if (Randoms.pickNumberInRange(START_NUM, END_NUM) < BASIS_NUM) {
+			return;
+		}
 		carPosition.movePosition();
+	}
+
+	public String displayCarStatus() {
+		return String.format(DISPLAY_FORMAT, carName.getName(), carPosition.displayPosition());
 	}
 }
